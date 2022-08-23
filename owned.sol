793ec13dba9bacb160107527ce1aa2b1f021667b
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-23 14:29:32 +0100

    faucet

--- owned.sol
+++ owned.sol
@@ -18,14 +18,21 @@ contract mortal is owned {
 }
 
 contract Faucet is mortal {
+    event Withdrawal(address indexed to, uint amount);
+    event Deposit(address indexed from, uint amount);
+
     // Give out ether to anyone who asks
     function withdraw(uint256 withdraw_amount) public {
         // Limit withdrawal amount
         require(withdraw_amount <= 0.1 ether);
+        require(this.balance >= withdraw_amount, "insufficient balance");
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
+        emit Withdrawal(msg.sender, withdraw_amount);
     }
 
     // Accept any incoming amount
-    function() public payable {}
+    function() public payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 }
