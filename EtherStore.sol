0f75277605a4283218c8f0b779f4027a618ea10e
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-06 11:07:47 +0100

    Ether store

---
+++ EtherStore.sol
@@ -0,0 +1,23 @@
+// an Ethereum vault that allows depositors to withdraw only 1 ether per week
+
+contract EtherStore {
+    uint256 public withdrawLimit = 1 ether;
+    mapping(address => uint256) public lastWithdrawTime;
+    mapping(address => uint256) public balances;
+
+    function depositFunds() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdrawFunds (uint256 _weiToWithdraw) public {
+        require(balances[msg.sender] >= _weiTowithdraw);
+
+        require(_weiToWithdraw <= withdrawLimit);
+
+        require(now >= lastWithdrawTime[msg.sender] + 1 weeks);
+        require(msg.sender.call.value(_weiToWithdraw)());
+        balances[msg.sender] -= _weiToWithdraw;
+
+        lastWithdrawTime[msg.sender] = now;
+    }
+}
